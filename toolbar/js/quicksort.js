174ccdc80e75d881aa41620086c5e63cff3182c8
yxfanxiao <1466014302@qq.com>
2015-07-02 11:00:46 +0800

    suddenly want to review quicksort

--- toolbar/js/quicksort.js
+++ toolbar/js/quicksort.js
@@ -1,24 +1,3 @@
-#ToolBar
-
-##Screenshot
-
-![](https://github.com/yxfanxiao/myFrontEndDemo/raw/master/toolbar/img/screenshot.png)
-
-## 1. use background pictures
-##### what i learn in this demo
-		* how to use `require.js`
-		* how to use `sass`
-		* how to modularize css 
-		* how to locate picture locations
-
-## 2. use font icon
-
-
-## 3. use pseudo-classes like 'before' & 'after' 
-
-
-## LOL review quicksort 
-```javascript
 function quicksort (array) {
 	sort(0, array.length-1);
 	function sort (pre, beh) {
@@ -48,5 +27,3 @@ function startSort () {
 	quicksort(array);
 	console.log(array);
 }
-
-```
